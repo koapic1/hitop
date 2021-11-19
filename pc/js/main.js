a76ae7ff6a62400f93700bd58c36df931e7c62a8
koapic1 <koapic1@gmail.com>
2021-11-19 14:31:37 +0900

    모바일연동

---
+++ pc/js/main.js
@@ -0,0 +1,10 @@
+const mainVisualSlider = new Swiper("#mainVisual", {
+    autoplay: true,
+    speed: 1000,
+    effect: "fade",
+    loop: true,
+    navigation: {
+        prevEl: "#mainVisual .prev",
+        nextEl: "#mainVisual .next",
+    },
+});

c8e3b0e101e72e98c26ec1fe7fe52618a8b229cf
koapic1 <koapic1@gmail.com>
2021-11-23 17:31:50 +0900

    main js 내용 정리

--- pc/js/main.js
+++ pc/js/main.js
@@ -12,10 +12,11 @@ const mainVisualSlider = new Swiper("#mainVisual", {
 const gnbItem = document.querySelectorAll("#gnb .list > li");
 //.querySelectorAll(찾는 것) : 배열처럼 생긴 nodeList를 리턴한다.
 const header = document.querySelector("#header");
-console.log(gnbItem.length);
+console.log(gnbItem.length); // 현재 gnbTtem의 length를 숫자로 출력 gnbItem = "#gnb .list > li"
 for (i = 0; i < gnbItem.length; i++) {
     gnbItem[i].addEventListener("mouseenter", function () {
-        header.classList.add("open");
+        //.addEventListener 해당 액션(mouseenter)에 이벤트를 추가
+        header.classList.add("open"); // 지정한 이벤트 발생시 open 이라는 class를 생성
     });
     gnbItem[i].addEventListener("mouseleave", function () {
         header.classList.remove("open");
